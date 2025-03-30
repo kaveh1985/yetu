d5f09ef3da3b782f3d1e0f45abbc80c0d32215ec
kaveh1985 <kaveh.ramzanzad@gamil.com>
2025-03-30 06:06:39 +0200

    photo changed

--- src/components/menu/Sandwich.js
+++ src/components/menu/Sandwich.js
@@ -5,16 +5,16 @@ const Sandwich = () => {
   return (
     <div className={styles.sandwichFrame}>
       <div className={styles.imageContainer}>
-        <img src="/food/sandw-5.jpg" alt="Sandwich 1" className={styles.sandwichImage} />
+        <img src="/food/food-5.jpg" alt="Sandwich 1" className={styles.sandwichImage} />
       </div>
       <div className={styles.imageContainer}>
-        <img src="/food/sandw-6.jpg" alt="Sandwich 2" className={styles.sandwichImage} />
+        <img src="/food/food-6.jpg" alt="Sandwich 2" className={styles.sandwichImage} />
       </div>
       <div className={styles.imageContainer}>
-        <img src="/food/sandw-7.jpg" alt="Sandwich 3" className={styles.sandwichImage} />
+        <img src="/food/food-7.jpg" alt="Sandwich 3" className={styles.sandwichImage} />
       </div>
       <div className={styles.imageContainer}>
-        <img src="/food/sandw-8.jpg" alt="Sandwich 4" className={styles.sandwichImage} />
+        <img src="/food/food-8.jpg" alt="Sandwich 4" className={styles.sandwichImage} />
       </div>
     </div>
   );
